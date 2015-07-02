68b35f8aa1fbb405cd076eb332869e4a16675a4a
Dwayne Thomas <thomas.dwayne@gmail.com>
2015-07-02 07:56:01 -0700

    json on bio started

---
+++ js/resumeBuilder.js
@@ -0,0 +1,18 @@
+{
+  "bio":
+{
+"name" : "Deets",
+"role" : "Webber",
+"contacts" : {
+      "mobile": "555-327-0231",
+      "email": "mdneue@yahoo.com",
+      "github": "https://github.com/dwaynethomas",
+      "twitter": "https://twitter.com/mdneue",
+      "location": "Portland, Oregon, USA"
+    },
+"welcomeMessage": "welcome to project 2",
+"skills": "shadowboxing",
+"biopic": "http://soccerandsoftwareunited.net",
+"display": "function"
+},
+}
